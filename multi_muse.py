9cff5f52b022711fa2f9948b71763b43c535a925
Benjamin De Leener <jackbdl@gmail.com>
2015-10-01 07:53:40 -0400

    NEW: multi muse!

---
+++ multi_muse.py
@@ -0,0 +1,52 @@
+__author__ = 'benjamindeleener'
+
+import sys
+import time
+from pymuse.ios import MuseServer
+from pymuse.viz import MuseViewerSignal, MuseViewerConcentrationMellow
+from pymuse.signals import MuseEEG, MuseConcentration, MuseMellow
+from liblo import ServerError
+
+
+def main():
+    # initialization of variables
+    signals, viewers = dict(), dict()
+
+    # EEG signal
+    #signal_eeg = MuseEEG(length=2000, acquisition_freq=220.0, do_fft=False)
+    #viewer_eeg = MuseViewerSignal(signal_eeg, 220.0, signal_boundaries=[600, 1200])
+
+    #signals['eeg'] = signal_eeg
+    #viewers['eeg'] = viewer_eeg
+
+    # Concentration and Mellow
+    signal_concentration = MuseConcentration(length=400, acquisition_freq=10.0)
+    signal_mellow = MuseMellow(length=400, acquisition_freq=10.0)
+    viewer_concentration_mellow = MuseViewerConcentrationMellow(signal_concentration, signal_mellow, signal_boundaries=[-0.05, 1.05])
+
+    signals['concentration'] = signal_concentration
+    signals['mellow'] = signal_mellow
+    viewers['concentration-mellow'] = viewer_concentration_mellow
+
+    # Initializing the server
+    try:
+        server = MuseServer(signal=signals, viewer=viewers)
+    except ServerError, err:
+        print str(err)
+        sys.exit(1)
+
+    # Displaying the viewers
+    for sign in viewers:
+        viewers[sign].show()
+
+    # Starting the server
+    try:
+        server.start()
+        while 1:
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        print "\nEnd of program: Caught KeyboardInterrupt"
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
