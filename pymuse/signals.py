8355ca430bc2e8bf1b103d44cf1784cd2aa641cc
Benjamin De Leener <jackbdl@gmail.com>
2016-07-13 19:42:47 -0400

    NEW: real acquisition frequency

--- pymuse/signals.py
+++ pymuse/signals.py
@@ -1,18 +1,28 @@
 __author__ = 'benjamindeleener'
-from numpy import linspace
+import numpy as np
 from datetime import datetime
 
 class MuseSignal(object):
     def __init__(self, length, acquisition_freq):
         self.length = length
         self.acquisition_freq = acquisition_freq
-        self.time = list(linspace(-float(self.length) / self.acquisition_freq + 1.0 / self.acquisition_freq, 0.0, self.length))
+        self.time = list(np.linspace(-float(self.length) / self.acquisition_freq + 1.0 / self.acquisition_freq, 0.0, self.length))
         self.init_time = datetime.now()
 
     def add_time(self):
         diff = datetime.now() - self.init_time
-        self.time.append(float(diff.total_seconds() * 1000))
+        self.time.append(float(diff.total_seconds() * 1000))  # in milliseconds
         del self.time[0]
+
+    def compute_real_acquisition_frequency(self, window=None):
+        if not window or window > self.length:
+            window = self.length
+
+        diff_time = np.zeros(window)
+        for k in range(0, window - 1):  # self.length, self.length - window, -1):
+            diff_time[k] = self.time[self.length - k] - self.time[self.length - (k + 1)]
+        acquisition_frequency = 1.0 / np.mean(diff_time)
+        return acquisition_frequency
 
 
 class MuseEEG(MuseSignal):
