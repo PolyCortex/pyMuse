8dc6d52a267ce9dd669cde72f93c7b7cd6c8df6f
Benjamin De Leener <jackbdl@gmail.com>
2016-11-14 06:56:11 -0500

    INST: changed order of dependencies

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib', 'numpy', 'scipy'],
+    install_requires=['matplotlib', 'scipy', 'numpy'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
