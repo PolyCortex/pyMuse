90dced409d1c491525024542010f54204569705e
Benjamin De Leener <jackbdl@gmail.com>
2017-06-06 19:42:11 -0400

    Improvements on interface

--- apps/EurekaInterface/boatGUI.py
+++ apps/EurekaInterface/boatGUI.py
@@ -4,11 +4,15 @@ from PyQt4 import QtGui, QtCore
 import thread
 import time
 from datetime import datetime
+import pexpect
+import serial
 
 from pymuse.ios import MuseIO, MuseIOError
 from pymuse.signals import MultiChannelSignal
 from pymuse.processes import Process
 from pymuse.pipeline import Analyzer
+
+MUSES = {'Muse 1': 'Muse-8BEF', 'Muse 2': 'Muse-9948', 'Muse 3': 'Muse-944A', 'Muse 4': 'Muse-7F68'}
 
 
 class Window(QtGui.QMainWindow):
@@ -63,6 +67,14 @@ class Window(QtGui.QMainWindow):
         self.exit_btn = QtGui.QPushButton("Quitter", self)
         self.exit_btn.resize(w_btn, h_btn)
         self.exit_btn.move(w/2-w_btn/2, h-2*h_btn)
+
+        self.Combo1 = QtGui.QComboBox(self)
+        self.Combo1.setObjectName("Muse Gauche")
+        for muse_name in MUSES:
+            self.Combo1.addItem(muse_name)
+        self.Combo1.resize(w_btn, h_btn)
+        self.Combo1.move(w / 4 - 1 * w_btn / 2, h - 1 * h_btn)
+
         self.connect1_btn = QtGui.QPushButton("Connecter J1", self)
         self.connect1_btn.resize(w_btn, h_btn)
         self.connect1_btn.move(w/4-3*w_btn/2, h-2*h_btn)
@@ -70,6 +82,14 @@ class Window(QtGui.QMainWindow):
         self.unconnect1_btn.resize(w_btn, h_btn)
         self.unconnect1_btn.move(w/4+w_btn/2, h-2*h_btn)
         self.unconnect1_btn.setDisabled(True)
+
+        self.Combo2 = QtGui.QComboBox(self)
+        self.Combo2.setObjectName("Muse Gauche")
+        for muse_name in MUSES:
+            self.Combo2.addItem(muse_name)
+        self.Combo2.resize(w_btn, h_btn)
+        self.Combo2.move(3*w / 4 - 1 * w_btn / 2, h - 1 * h_btn)
+
         self.connect2_btn = QtGui.QPushButton("Connecter J2", self)
         self.connect2_btn.resize(w_btn, h_btn)
         self.connect2_btn.move(3*w/4-3*w_btn/2, h-2*h_btn)
@@ -77,19 +97,34 @@ class Window(QtGui.QMainWindow):
         self.unconnect2_btn.resize(w_btn, h_btn)
         self.unconnect2_btn.move(3*w/4+w_btn/2, h-2*h_btn)
         self.unconnect2_btn.setDisabled(True)
+
         self.play_btn = QtGui.QPushButton("Jouer", self)
         self.play_btn.resize(w_btn, h_btn)
         self.play_btn.move(w/2-w_btn/2, h/2)
+        self.is_playing = False
+
+        self.stop_btn = QtGui.QPushButton("Stop", self)
+        self.stop_btn.resize(w_btn, h_btn)
+        self.stop_btn.move(w / 2 - w_btn / 2, h / 2 + 2 * h_btn)
+        self.stop_btn.setDisabled(True)
+
         # Associate callbacks
         self.exit_btn.clicked.connect(self.cb_close_app)
         self.play_btn.clicked.connect(self.cb_play)
+        self.stop_btn.clicked.connect(self.cb_stop)
         self.connect1_btn.clicked.connect(self.cb_connect1)
         self.unconnect1_btn.clicked.connect(self.cb_unconnect1)
         self.connect2_btn.clicked.connect(self.cb_connect2)
         self.unconnect2_btn.clicked.connect(self.cb_unconnect2)
-
         self.change_image_P1.connect(self.update_data_P1)
         self.change_image_P2.connect(self.update_data_P2)
+
+        # variables
+        self.connexion_muse_P1 = None
+        self.connexion_muse_P2 = None
+        self.serial_channel = None
+        self.port = '/dev/'  # this line must be changed depending on the computer
+        self.baud = 9600
 
         # Display window
         self.showFullScreen()
@@ -97,6 +132,12 @@ class Window(QtGui.QMainWindow):
     def cb_close_app(self):
         # Make sure the Muse are disconnected
         # Close the GUI
+        if self.connexion_muse_P1 is not None:
+            self.connexion_muse_P1.kill()
+        if self.connexion_muse_P2 is not None:
+            self.connexion_muse_P2.kill()
+        if self.serial_channel is not None:
+            self.serial_channel.close()
         sys.exit()
 
     def cb_play(self):
@@ -109,39 +150,125 @@ class Window(QtGui.QMainWindow):
         # Needs to run for a set amount of time OR until a player has activated the contact detector at the finish line
         # At the end of execution, needs to put back the GUI in its initial state whilst keeping the Muse connected
         # Also, should verify if both Muse are connected or just one, since the game should be playable alone if needed
-        print(" ")  # Dummy print
+        # init port
+        try:
+            self.serial_channel = serial.Serial()
+            self.serial_channel.port = self.port
+            self.serial_channel.baudrate = self.baud
+            self.serial_channel.readable()
+            self.serial_channel.timeout = 1
+            # open port
+            self.serial_channel.open()
+            # print(ser.is_open)
+        except Exception as e:
+            print e
+            print 'could not connect to port ' + self.port
+            self.is_playing = False
+            self.play_btn.setStyleSheet("background-color: red")
+            return
+
+        self.is_playing = True
+        self.play_btn.setStyleSheet("background-color: green")
+        self.play_btn.setDisabled(True)
+        self.connect1_btn.setEnabled(True)
+
+    def cb_stop(self):
+        self.is_playing = False
+        if self.serial_channel is not None:
+            self.serial_channel.close()
+        self.play_btn.setStyleSheet("background-color: grey")
+        self.connect1_btn.setEnabled(True)
+        self.play_btn.setDisabled(True)
 
     def cb_connect1(self):
         # Needs to connect Muse to the program here
         # Verify that the connection is good
         # If the connection has failed, show an error message
         # If the connection is successful, disable the connect button and activate the unconnect one
-        self.connect1_btn.setDisabled(True)
-        self.unconnect1_btn.setEnabled(True)
+        muse_P1 = MUSES[str(self.Combo1.currentText())]
+        cmd = 'muse-io --osc osc.udp://localhost:5001 --device ' + muse_P1
+        print cmd
+        try:
+            self.connexion_muse_P1 = pexpect.spawn(cmd)
+            index = self.connexion_muse_P1.expect(['Connected.', 'Connection failure 5', pexpect.EOF, pexpect.TIMEOUT])
+            if index >= 1:
+                print index
+                connected = False
+                self.connexion_muse_P1 = None
+            else:
+                connected = True
+        except Exception as e:
+            print e
+            connected = False
+            self.connexion_muse_P1 = None
+
+        if connected:
+            self.Combo1.setDisabled(True)
+            self.connect1_btn.setDisabled(True)
+            self.unconnect1_btn.setEnabled(True)
+            self.connect1_btn.setStyleSheet("background-color: green")
+        else:
+            self.connect1_btn.setStyleSheet("background-color: red")
+
 
     def cb_unconnect1(self):
         # Needs to disconnect the Muse and verify that the ports have been closed
         # If the disconnection is successful, disable the unconnect button and activate the connect one
+        self.connexion_muse_P1.sendcontrol('c')
+        self.connexion_muse_P1 = None
+        print 'Killed process Muse P1'
+
+        self.Combo1.setEnabled(True)
         self.connect1_btn.setEnabled(True)
         self.unconnect1_btn.setDisabled(True)
+        self.connect1_btn.setStyleSheet("background-color: gray")
 
     def cb_connect2(self):
         # Needs to connect Muse to the program here
         # Verify that the connection is good
         # If the connection has failed, show an error message
         # If the connection is successful, disable the connect button and activate the unconnect one
-        self.connect2_btn.setDisabled(True)
-        self.unconnect2_btn.setEnabled(True)
+        muse_P2 = MUSES[str(self.Combo2.currentText())]
+        cmd = 'muse-io --osc osc.udp://localhost:5002 --device ' + muse_P2
+        print cmd
+        try:
+            self.connexion_muse_P2 = pexpect.spawn(cmd)
+            index = self.connexion_muse_P2.expect(['Connected.', 'Connection failure 5', pexpect.EOF, pexpect.TIMEOUT])
+            if index >= 1:
+                print index
+                connected = False
+                self.connexion_muse_P2 = None
+            else:
+                connected = True
+        except Exception as e:
+            print e
+            connected = False
+            self.connexion_muse_P2 = None
+
+        if connected:
+            self.Combo2.setDisabled(True)
+            self.connect2_btn.setDisabled(True)
+            self.unconnect2_btn.setEnabled(True)
+            self.connect2_btn.setStyleSheet("background-color: green")
+        else:
+            self.connect2_btn.setStyleSheet("background-color: red")
 
     def cb_unconnect2(self):
         # Needs to disconnect the Muse and verify that the ports have been closed
         # If the disconnection is successful, disable the unconnect button and activate the connect one
+        self.connexion_muse_P2.sendcontrol('c')
+        self.connexion_muse_P2 = None
+        print 'Killed process Muse P2'
+
+        self.Combo2.setEnabled(True)
         self.connect2_btn.setEnabled(True)
         self.unconnect2_btn.setDisabled(True)
 
     @QtCore.pyqtSlot(float)
     def update_data_P1(self, data):
-        print 'signal received P1', data
+        if not self.is_playing:
+            return
+
         if 0.0 <= data < 0.3:
             self.speed1_label.setPixmap(self.speed1_pixmap_1)
         if 0.17 <= data < 0.33:
@@ -154,11 +281,22 @@ class Window(QtGui.QMainWindow):
             self.speed1_label.setPixmap(self.speed1_pixmap_5)
         if 0.83 <= data <= 1:
             self.speed1_label.setPixmap(self.speed1_pixmap_6)
+
+        if self.serial_channel is not None:
+            value = int(100 + data * 255)
+            try:
+                self.serial_channel.write(value)  # between 100 and 355
+            except Exception as e:
+                print e
+                self.is_playing = False
+
         return
 
     @QtCore.pyqtSlot(float)
     def update_data_P2(self, data):
-        print 'signal received P2', data
+        if not self.is_playing:
+            return
+
         if 0.0 <= data < 0.3:
             self.speed2_label.setPixmap(self.speed2_pixmap_1)
         if 0.17 <= data < 0.33:
@@ -171,11 +309,19 @@ class Window(QtGui.QMainWindow):
             self.speed2_label.setPixmap(self.speed2_pixmap_5)
         if 0.83 <= data <= 1:
             self.speed2_label.setPixmap(self.speed2_pixmap_6)
+
+        if self.serial_channel is not None:
+            value = int(400 + data * 255)
+            try:
+                self.serial_channel.write(value)  # between 400 and 655
+            except Exception as e:
+                print e
+                self.is_playing = False
+
         return
 
 
 def update_data(update_frequency=20.0, gui=None, signal=None, lock=None, player='P1'):
-    update_timing = 1.0 / float(update_frequency)
     if gui is not None:
         times_web = 0.0
         while True:
@@ -194,9 +340,9 @@ def update_data(update_frequency=20.0, gui=None, signal=None, lock=None, player=
                     gui.change_image_P1.emit(s)
                 elif player == 'P2':
                     gui.change_image_P2.emit(s)
-                print 'I am updating my data', s, analysis_frequency
+                #print 'I am updating my data', s, analysis_frequency
             else:
-                time.sleep(0.5 / update_frequency)
+                time.sleep(0.2 / update_frequency)
 
 
 def run_server(gui=None, port=5001, player='P1'):
