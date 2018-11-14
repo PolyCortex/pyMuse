f7ee9aa87ed4d2970b47ba50edd101de47d984c3
William Harvey <william.harvey@polymtl.ca>
2018-11-13 23:17:01 -0500

    Creation d'une premisse de la classe signal

--- pymuse/input_stream.py
+++ pymuse/input_stream.py
@@ -4,33 +4,38 @@ from Queue import Queue
 
 DEFAULT_PORT = 5001
 LOCALHOST = '127.0.0.1'
+DEFAULT_SIGNAL_QUEUE_LENGTH = 1024
+DEFAULT_MUSE_EEG_ACQUISITION_FREQUENCY = 256
+DEFAULT_MUSE_BATT_ACQUISITION_FREQUENCY = 10
 
 class MuseInputStream():
     def __init__(self, port = DEFAULT_PORT, ip = LOCALHOST):
         self.signals = dict()
-
-        dispatcher = dispatcher.Dispatcher()
-        dispatcher.map("/muse/eeg", callback, 'eeg')
+        disp = dispatcher.Dispatcher()
+        disp.map("/eeg", self.callback, DEFAULT_MUSE_EEG_ACQUISITION_FREQUENCY, 'eeg')
+        disp.map("/notch_filtered_eeg", self.callback, DEFAULT_MUSE_EEG_ACQUISITION_FREQUENCY, 'notch_filtered_eeg')
+        disp.map("/batt", self.callback, DEFAULT_MUSE_BATT_ACQUISITION_FREQUENCY, 'batt')
         server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
         server.serve_forever()
-    
-    def callback(self, *args): #Le dernier argument sera la signalName
-        for i, arg in args[::-1]:
-            if (i == 0):
-                signal_name = arg
-            else:
-                if(signal_name not in self.signals):
-                    self.signals[signal_name] = Signal()
-                self.signals[signal_name].addData(arg)
-                
 
-                
+    def callback(self, *args):
+        data = []
+        signal_name = args[-1]
+        acquisition_frequency = args[-2]
+        for arg in args[0:-2]:
+            if(signal_name not in self.signals):
+                self.signals[signal_name] = Signal(DEFAULT_SIGNAL_QUEUE_LENGTH, acquisition_frequency)
+            data.append(arg)
+        self.signals[signal_name].push(data)
 
 class Signal():
-    def __init__(self, length):
+    def __init__(self, length, acquisition_frequency):
         self.init_time = datetime.now()
         self.data = Queue(length)
-    
-    def add(self, *args):
-        
+        self.acquisition_period = 1/acquisition_frequency
 
+    def push(self, data):
+        self.data.put(data, True, self.acquisition_period)
+    
+    def pop(self):
+        return self.data.get()
