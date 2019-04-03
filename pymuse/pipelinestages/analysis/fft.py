c0d1801a70d7fea5457756791efb4e0ff4fd64e2
William Harvey <william.harvey@polymtl.ca>
2019-04-03 01:56:27 -0400

    Takes in consideration the multiple channels of the signal

--- pymuse/pipelinestages/analysis/fft.py
+++ pymuse/pipelinestages/analysis/fft.py
@@ -19,34 +19,51 @@ class FFT(PipelineStage):
 
     def __init__(self, window_size: int = DEFAULT_WINDOW_SIZE, detrending_method: str = None, scaling_type: ScalingType = ScalingType.AMPLITUDE):
         super().__init__()
-        self.buffer_window = np.empty(window_size)
-        self.detrending_method = detrending_method
+        self._window_size = window_size
+        self._buffer_window = None
+        self._detrending_method = detrending_method
         if isinstance(scaling_type, ScalingType):
-            self.scaling_type = scaling_type
+            self._scaling_type = scaling_type
         else:
-            self.scaling_type = ScalingType.AMPLITUDE
+            self._scaling_type = ScalingType.AMPLITUDE
 
     def _execute(self):
         self._fill_window()
-        if (self.detrending_method is not(None)):
+        if (self._detrending_method is not(None)):
             self._detrend()
 
         # Overlapping with Hamming Window and Square window and Welch's method
 
-        self._write_queues_out(self._fft())
+        self._write_queues_out(self._transform_to_freq())
 
     def _fill_window(self):
-        for i in len(self.buffer_window):
-            self.buffer_window[i] = self._queue_in.get()
+        data = self._queue_in.get()
+        self._buffer_window = np.empty(
+            (self._window_size, len(data[1])))  # Allocate a buffer window
+        self._add_data_to_buffer_window(0, data)
+        # From 1 since we already consumed a data
+        for i in range(1, len(self._buffer_window)):
+            data = self._queue_in.get()
+            self._add_data_to_buffer_window(i, data)
+
+    def _add_data_to_buffer_window(self, index, data):
+        for channelIndex in len(data[1]):
+            self._buffer_window[index][channelIndex] = data[1][channelIndex]
 
     def _detrend(self):
-        self.buffer_window = scipy.signal.detrend(
-            self.buffer_window, type=self.detrending_method)
+        for i in len(self._buffer_window):
+            self._buffer_window[i] = scipy.signal.detrend(
+                self._buffer_window, type=self._detrending_method)
 
-    def _fft(self):
-        if (self.scaling_type == ScalingType.AMPLITUDE):
-            return 2*np.abs(scipy.fftpack.fft(self.buffer_window))/len(self.buffer_window)
+    def _transform_to_freq(self):
+        ffts = []
+        for buffer_window in self._buffer_window:
+            ffts.append(self._fft(buffer_window))
+        return ffts
+
+    def _fft(self, window):
+        if (self._scaling_type == ScalingType.AMPLITUDE):
+            return 2*np.abs(scipy.fftpack.fft(window))/len(window)
         else:
-            return np.abs(scipy.fftpack.fft(self.buffer_window) /
-                          len(self.buffer_window)) ** 2
-
+            return np.abs(scipy.fftpack.fft(window) /
+                          len(window)) ** 2
